6f7f41bee9f579a276db3090dee4c1e6d7924980
yr <1070591579@qq.com>
2017-07-26 15:39:59 +0800

    users管理界面

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,52 @@ var express = require('express');
 var router = express.Router();
 var conn = require('../mysql/db1').getConn();
 
+// /users/login
+router.post('/login',(req,res)=>{
+    var sql = 'select * from users where name= ? and password=? ';
+    conn.query(sql,[req.body.name,req.body.password], function(err,results){
+        console.log(req.body);
+        if(err){
+            console.log(err);
+            res.send({success:false,msg:'服务器发生内部错误'})
+            console.log(req.body);
+        }else{
+            if(results.length>0){
+                res.send({success:true,data:results[0]})
+            }else{
+                res.send({success:false,msg:'用户名和密码不匹配'})
+            }
+        }
+    });
+})
+
+//users/register
+router.post('/register',(req,res)=>{
+    var sql = `INSERT INTO users (name,password,phone) VALUE (?,?,?)`;
+    conn.query(sql,[req.body.name,req.body.password,req.body.phone], function(err,results){
+        //console.log(req.body);
+        if(err){
+            console.log(err);
+            res.send({success:false,msg:'服务器发生内部错误'})
+        }else{
+            console.log({success:true,msg:'注册成功'});
+            res.send({success:true});
+        }
+    });
+})
+''
+//users/lostpwd
+router.put('/lostpwd',(req,res)=>{
+    var sql = `UPDATE users SET password=? WHERE phone=?`;
+    conn.query(sql,[req.body.password,req.body.phone],(err,result)=>{
+        console.log(req.body);
+        if(!err && result.affectedRows>0){
+            return res.json({success:true,data:result.insertId});
+        }else{
+            res.json({success:false,data:'修改密码失败'});
+        }
+    })
+})
 
 router.get('/',(req, res)=>{
     var sql = `SELECT * FROM users`;
